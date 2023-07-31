3d721dd11f0f7a25bc5a35edce4dc16bddb0fe1d
lerkor-online <lerkor.online@gmail.com>
2023-07-31 19:36:58 -0300

    update dashboard fix

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,23 +1,17 @@
-import React from "react";
+import React from 'react';
 
-//import { ChartCard } from "../../components/Dashboard/ChartCard";
-// import { TableCard } from "../../components/Dashboard/TableCard";
-// import { RecentActivity } from "../../components/Dashboard/RecentActivity";
+/* import { ChartCard } from '../../components/Dashboard/ChartCard';
+import { TableCard } from '../../components/Dashboard/TableCard';
+import { RecentActivity } from '../../components/Dashboard/RecentActivity'; */
 
 const Dashboard = () => {
-  return (
-    <div>
-      <h2>Dashboard</h2>
-      {
-        <>
-          {/* <ChartCard /> */}
-          {/* <TableCard /> */}
-          {/* <RecentActivity /> */}
-        </>
-      }
-      {/* Aquí puedes agregar el contenido del dashboard */}
-    </div>
-  );
-};
+    return (
+      <div>
+        <h2>Dashboard</h2>
+        {<><ChartCard /><TableCard /><RecentActivity /></>}
+        {/* Aquí puedes agregar el contenido del dashboard */}
+      </div>
+    );
+  };
 
 export default Dashboard;
