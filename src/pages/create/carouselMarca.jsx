4d95f5feba71cef225a1e18726368ae410bfc777
Hengers3012 <hengersrosario.m@gmail.com>
2023-08-06 01:38:32 -0400

    feat: Conponent stock. || fix: Conponent addCars

--- src/pages/create/carouselMarca.jsx
+++ src/pages/create/carouselMarca.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/jsx-key */
 import React from "react";
-import json from "./createdata.json";
+import json from "./logos.json";
 function CarouselMarca() {
   return (
     <div className="flex flex-col p-4 justify-center items-center bg-slate-50">
