7c05a98a1b905810f264f3ccab9bb12522a9f523
Wilialberth Jiménez <7jimenez.w@gmail.com>
2023-08-07 23:58:56 -0300

    dashboard_users

---
+++ src/pages/dashboard_1/pages/UserTable.jsx
@@ -0,0 +1,31 @@
+/* import React from "react";
+
+function UserTable() {
+  const users = [
+    { id: 1, name: "Juan Pérez", mail: "juan@example.com" },
+    { id: 2, name: "Juana Sanchez", mail: "juana@example.com" },
+    { id: 3, name: "Pedro López", mail: "pedro@example.com" },];
+
+  return (
+    <table className="w-full border-collapse">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Mail</th>
+        </tr>
+      </thead>
+      <tbody>
+        {users.map((user) => (
+          <tr key={user.id}>
+            <td>{user.id}</td>
+            <td>{user.name}</td>
+            <td>{user.mail}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+export default UserTable; */
--- src/pages/dashboard_1/pages/Users.jsx
+++ src/pages/dashboard_1/pages/Users.jsx
@@ -1,12 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import './Users.css';
+import axios from 'axios';
 
 function Users() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://localhost:3001/users')
+      .then(response => {
+        setUsers(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
   return (
     <div className="flex flex-col m-auto min-h-full min-w-full ">
       <div className="min-h-[6vh]">
         <h1>USERS</h1>
       </div>
-      <div className="border-2 text-black bg-slate-300 rounded-lg mt-2 min-h-[91vh]"></div>
+      <div className="table-container">
+        <table id="data-table">
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Rol</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map(user => (
+              <tr key={user.id}>
+                <td>{user.id}</td>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>{user.rol}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
