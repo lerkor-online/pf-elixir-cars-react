8a27b4f590084adac730ce22f09c1b52a2a88cae
Hengers3012 <hengersrosario.m@gmail.com>
2023-08-09 09:36:19 -0400

    update component

--- src/pages/dashboard_1/components/AdminUsers/AdminUsers.jsx
+++ src/pages/dashboard_1/components/AdminUsers/AdminUsers.jsx
@@ -5,25 +5,40 @@ import Card_Users from "../Cards/Card_Users";
 function AdminUsers() {
   const [users, setUsers] = useState([]);
   const [selectedUsers, setSelectedUsers] = useState([]);
+  const [showDeleteButton, setShowDeleteButton] = useState(false);
 
-  useEffect(() => {
-    const fetchUsersByEmail = async () => {
-      try {
-        const response = await axios.get("http://localhost:3001/users");
+  const fetchUser = async () => {
+    try {
+      const response = await axios.get("http://localhost:3001/users");
 
-        const jsonData = await response;
-        console.log(jsonData);
-        if (Array.isArray(jsonData)) {
-          setUsers(jsonData);
-        } else {
-          console.log("Response is not an array");
-        }
-      } catch (error) {
-        console.log("Error fetching data:", error);
+      const jsonData = await response.data.users;
+      console.log([jsonData]);
+
+      if (Array.isArray([jsonData]) && jsonData.length > 0) {
+        setUsers(jsonData);
+      } else {
+        console.log("No hay usuarios");
       }
-    };
-    fetchUsersByEmail();
-  }, []);
+    } catch (error) {
+      console.log("Error fetching data:", error);
+    }
+  };
+
+  if (users.length === 0) {
+    fetchUser();
+  }
+
+  const handlerUserEdit = async (email) => {
+    console.log(email);
+    try {
+      const response = await axios.get(
+        `http://localhost:3001/user?email=${email}`
+      );
+      console.log(response);
+    } catch (error) {
+      console.log("Error al editar el usuario", error);
+    }
+  };
 
   const handleCheckUser = (e) => {
     const userId = e.target.value;
@@ -35,52 +50,98 @@ function AdminUsers() {
     }
   };
 
+  useEffect(() => {
+    if (selectedUsers.length > 0) {
+      setShowDeleteButton(true);
+    } else {
+      setShowDeleteButton(false);
+    }
+  }, [selectedUsers]);
+
+  const handleDeleteUsers = async () => {
+    console.log(selectedUsers);
+
+    try {
+      const response = await axios.delete(
+        `http://localhost:3001/users/${selectedUsers}`,
+        {
+          data: { ids: selectedUsers },
+        }
+      );
+      console.log(response);
+
+      console.log("El usuario se ha borrado correctamente.");
+    } catch (error) {
+      console.log("Error al borrar el usuario", error);
+    } finally {
+      console.log("Usuario(s) Borrado(s)", selectedUsers);
+      fetchUser();
+      setSelectedUsers([]);
+    }
+  };
+
   return (
     <div className="text-gray-900 bg-gray-200">
-      <div className="p-4 flex">
-        <h1 className="text-3xl">Users</h1>
+      <div className="flex flex-row">
+        <div className="p-4 flex">
+          <h1 className="text-3xl">Users</h1>
+        </div>
+        {showDeleteButton && (
+          <div className="ml-auto mr-4 mt-auto ">
+            <button
+              className="mt-4 bg-red-500 hover:bg-red-600 text-white font-semibold py-2 px-4 rounded"
+              onClick={handleDeleteUsers}
+            >
+              Delete User(s)
+            </button>
+          </div>
+        )}
       </div>
-      <div className="w-auto">
-        <table className="min-w-full">
-          <thead className="bg-gray-200 border-b-2 border-gray-200">
-            <tr className="">
-              <th className=" w-10 p-3 text-sm font-semibold tracking-wide text-left">
-                ID
-              </th>
-              <th className="p-3 text-sm font-semibold tracking-wide text-left">
-                NAME
-              </th>
-              <th className="p-3 text-sm font-semibold tracking-wide text-left">
-                EMAIL
-              </th>
-              <th className="p-3 text-sm font-semibold tracking-wide text-left">
-                STATUS
-              </th>
-              <th className="p-3 text-sm font-semibold tracking-wide text-left">
-                ROL
-              </th>
-              <th className="w-5 p-3 text-sm font-semibold tracking-wide text-left">
-                Select
-              </th>
-            </tr>
-          </thead>
-
-          <tbody>
-            {users &&
-              users.map((user, index) => (
-                <Card_Users
-                  key={index}
-                  user={user.id}
-                  handleCheckUser={handleCheckUser}
-                />
-              ))}
-          </tbody>
-        </table>
-        {/* <div className="m-2">ID</div>
-        <div className="m-2">NAME</div>
-        <div className="m-2">EMAIL</div>
-        <div className="m-2">STATUS</div>
-        <div className="m-2">ROL</div> */}
+      <div className="flex flex-col">
+        <div className="w-full ">
+          <div className="m-5 border-b border-gray-200 rounded-sm shadow">
+            <table className="min-w-full divide-y-2 divide-[#f1d72f]">
+              <thead className="bg-gray-50 ">
+                <tr className="">
+                  <th className="w-10 p-3 text-sm font-semibold tracking-wide text-left">
+                    ID
+                  </th>
+                  <th className="p-3 text-sm font-semibold tracking-wide text-left">
+                    NAME
+                  </th>
+                  <th className="p-3 text-sm font-semibold tracking-wide text-left">
+                    EMAIL
+                  </th>
+                  <th className="p-3 text-sm font-semibold tracking-wide text-left">
+                    ROLE
+                  </th>
+                  <th className="p-3 text-sm font-semibold tracking-wide text-left">
+                    EDIT
+                  </th>
+                  <th className="w-5 p-3 text-sm font-semibold tracking-wide text-left">
+                    SELECT
+                  </th>
+                </tr>
+              </thead>
+              {users.length === 0 ? (
+                <div className="min-w-full bg-white divide-y divide-gray-300">
+                  Loading...
+                </div>
+              ) : (
+                <tbody className="bg-white divide-y divide-gray-300">
+                  {users?.map((user, index) => (
+                    <Card_Users
+                      key={index}
+                      user={user}
+                      handlerUserEdit={handlerUserEdit}
+                      handleCheckUser={handleCheckUser}
+                    />
+                  ))}
+                </tbody>
+              )}
+            </table>
+          </div>
+        </div>
       </div>
     </div>
   );
--- src/pages/dashboard_1/components/Cards/Card_Users.jsx
+++ src/pages/dashboard_1/components/Cards/Card_Users.jsx
@@ -1,27 +1,56 @@
 import React from "react";
 
-function Card_Users({ user, handleCheckUser }) {
+function Card_Users({ user, handleCheckUser, handlerUserEdit }) {
   return (
-    <tr>
+    <tr className="whitespace-nowrap border-b-2">
       <td className="p-3 text-sm font-mono tracking-wide text-left">
-        {user.id}
+        <div>{user.id}</div>
       </td>
+
       <td className="p-3 text-sm font-mono tracking-wide text-left">
-        {user.name}
+        <div>{user.name}</div>
       </td>
+
       <td className="p-3 text-sm font-semibold tracking-wide text-left">
-        <a href="http://" target="_blank" rel="noopener noreferrer">
-          {user.email}
-        </a>
+        <div>
+          <a href="http://" target="_blank" rel="noopener noreferrer">
+            {user.email}
+          </a>
+        </div>
       </td>
+
       <td className="p-3 text-sm font-mono tracking-wide text-left">
-        {user.status}
+        <div>{user.role}</div>
       </td>
-      <td className="p-3 text-sm font-mono tracking-wide text-left">
-        {user.role}
+
+      <td className="px-4 py-4">
+        <button
+          className="w-fit h-fit p-0 border-0 text-white bg-white font-semibold rounded"
+          onClick={() => handlerUserEdit(user.email)}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="w-6 h-6 text-blue-500"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 
+                      2 0 112.828 
+                      2.828L11.828 15H9v-2.828l8.586-8.586z"
+            />
+          </svg>
+        </button>
       </td>
+
       <td className="p-3 text-sm tracking-wide text-center">
-        <input type="checkbox" value={user.id} onChange={handleCheckUser} />
+        <div>
+          <input type="checkbox" value={user.id} onChange={handleCheckUser} />
+        </div>
       </td>
     </tr>
   );
