b74f992187be089ec13a3e1b6e1e788bf4e87935
Wilialberth Jiménez <7jimenez.w@gmail.com>
2023-08-08 23:18:25 -0300

    paypal is working

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {useState, useEffect } from 'react';
+/* import {useState, useEffect } from 'react'; */
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import RootLayout from "./components/Helpers/RootLayout";
 
@@ -12,28 +12,34 @@ import About from "./pages/about/About";
 import Detail from "./pages/detail/detail";
 import OurTeam from "./pages/ourteam/Ourteam";
 import Contact from "./pages/contact/Contact";
-import Checkout from "./pages/checkout/Checkout";
+/* import Checkout from "./pages/checkout/Checkout"; */
 import Dashboard from "../src/components/Dashboard/Dashboard";
 import AdminDashboard from "./pages/dashboard_1/AdminDashboard";
+import { PayPalButton } from "./components/PaypalButton/PaypalButton";
+
 
 function App() {
   const root = createBrowserRouter([
     {
       path: "/",
       element: <RootLayout />,
-      /* errorElement: <Error />, */
+     /*  errorElement: <Error />, */
       children: [
         { path: "/", element: <LandingPage/> },
         { path: "/home", element: <Home/> },
         { path: "/categoria-producto/0km", element: <Cerokm/> },
         { path: "/categoria-producto/0km/detail/:id", element: <Detail/> },
+        {
+          path: "/paypal-button/:precio/:nombre",
+          element: <PayPalButton />,
+        },
         { path: "/about", element: <About/> },
         { path: "/about/ourteam", element: <OurTeam/> },
         { path: "/about/contact", element: <Contact/> },
         { path: "/create", element: <AddCars/> },
         { path: "/dashboard", element: <Dashboard/> },
         { path: "/dashboard_1/*", element: <AdminDashboard /> },
-        { path: "/checkout", element: <Checkout/> },
+        /* { path: "/checkout", element: <Checkout/> }, */
       ],
     },
   ]);
@@ -46,4 +52,55 @@ function App() {
   )
 }
 
-export default App
+export default App 
+
+/* import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import RootLayout from "./components/Helpers/RootLayout";
+
+import "./App.css";
+import Home from "./pages/home/home";
+import Cerokm from "./pages/categoria-producto/0km/Cerokm";
+import LandingPage from "./components/landing/LandingPage";
+import AddCars from "./pages/create/AddCars";
+import About from "./pages/about/About";
+import Detail from "./pages/detail/detail";
+import OurTeam from "./pages/ourteam/Ourteam";
+import Contact from "./pages/contact/Contact";
+import Dashboard from "../src/components/Dashboard/Dashboard";
+import AdminDashboard from "./pages/dashboard_1/AdminDashboard";
+import { PayPalButtonComponent } from "./components/PaypalButton/PaypalButton";
+
+function App() {
+  const root = createBrowserRouter([
+    {
+      path: "/",
+      element: <RootLayout />,
+      errorElement: <Error />, 
+      children: [
+        { path: "/", element: <LandingPage /> },
+        { path: "/home", element: <Home /> },
+        { path: "/categoria-producto/0km", element: <Cerokm /> },
+        { path: "/categoria-producto/0km/detail/:id", element: <Detail /> },
+        {
+          path: "/paypal-button/:precio/:nombre",
+          element: <PayPalButtonComponent />,
+        },
+        { path: "/about", element: <About /> },
+        { path: "/about/ourteam", element: <OurTeam /> },
+        { path: "/about/contact", element: <Contact /> },
+        { path: "/create", element: <AddCars /> },
+        { path: "/dashboard", element: <Dashboard /> },
+        { path: "/dashboard_1/*", element: <AdminDashboard /> },
+      ],
+    },
+  ]);
+  return (
+    <>
+      <div className="App">
+        <RouterProvider router={root} />
+      </div>
+    </>
+  );
+}
+
+export default App; */
--- src/components/PaypalButton/PaypalButton.jsx
+++ src/components/PaypalButton/PaypalButton.jsx
@@ -1,4 +1,63 @@
 import React from 'react';
+import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
+
+export function PayPalButton({ precio, nombre }) {
+  const clientId = "AVEb_6nyKhKDoS1J27I2AiAdYH3r0qD-GUwkHptOALusT7-kTlhjgG8adJFy39QVrt80CiozaAFb6P3u";
+
+  // Manejar la lógica después de un pago exitoso
+  const handlePaymentSuccess = (details, data) => {
+    console.log("Pago realizado con éxito:", details);
+  };
+
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '50vh' }}>
+      <PayPalScriptProvider options={{ "client-id": clientId }}>
+        <div style={{ width: '50%' }}>
+          <PayPalButtons
+            style={{ layout: 'vertical' }}
+            createOrder={(_data, actions) => {
+              // Lógica para crear la orden de pago
+              return actions.order.create({
+                application_context: {
+                    // Orden exitosa
+                    return_url: "http://localhost:3000/complete",
+                    // Orden cancelada
+                    cancel_url: "http://localhost:3000/cancel",
+                },
+                purchase_units: [
+                  {
+                    reference_id: "Compra de prueba",
+                    description: `Compra de ${nombre}`,
+                    amount: {
+                      value: precio,
+                      currency_code: 'USD', // Cambiar a la moneda deseada
+                    },
+                  },
+                ],
+              });
+            }}
+            onApprove={(data, actions) => {
+              // Lógica después de que el usuario apruebe el pago
+              return actions.order.capture().then(function (details) {
+                handlePaymentSuccess(details, data);
+              });
+            }}
+          />
+        </div>
+      </PayPalScriptProvider>
+    </div>
+  );
+}
+
+export default PayPalButton;
+
+
+
+
+
+
+
+/* import React from 'react';
 import { PayPalScriptProvider, PayPalButtons } from '@paypal/react-paypal-js';
 
 const PayPalButton = () => {
@@ -38,57 +97,4 @@ const PayPalButton = () => {
   );
 };
 
-export default PayPalButton;
-
-
-/* import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
-
-
-// Componente donde se muestra el botón de pago que basado en sus props define el monto y el product a cobrar
-
-// Componente donde se muestra el botón de pago
-export function PayPalButtonComponent({ precio, nombre }) {
-  const clientId = "AVEb_6nyKhKDoS1J27I2AiAdYH3r0qD-GUwkHptOALusT7-kTlhjgG8adJFy39QVrt80CiozaAFb6P3u";
-
-  // Manejar la lógica después de un pago exitoso
-  const handlePaymentSuccess = (details, data) => {
-    console.log("Pago realizado con éxito:", details);
-  };
-
-  return (
-    <PayPalScriptProvider options={{ "client-id": clientId }}>
-      <PayPalButtons
-        createOrder={(data, actions) => {
-          // Lógica para crear la orden de pago
-          return actions.order.create({
-            application_context: {
-                //Orden exitosa
-                return_url: "http://localhost:3000/complete",
-                //orden cancelada
-                cancel_url: "http://localhost:3000/cancel",
-            },
-            purchase_units: [
-              {
-                reference_id: "Compra de prueba ",
-                description: `Compra de ${nombre}`,
-                amount: {
-                    value: 200,
-                item_total: {
-                  value: precio, // Cambiar al valor que desees cobrar
-                }
-                },
-              },
-            ],
-          });
-        }}
-        onApprove={(data, actions) => {
-          // Lógica después de que el usuario apruebe el pago
-          return actions.order.capture().then(function (details) {
-            handlePaymentSuccess(details, data);
-          });
-        }}
-      />
-    </PayPalScriptProvider>
-  );
-}
- */
+export default PayPalButton; */
