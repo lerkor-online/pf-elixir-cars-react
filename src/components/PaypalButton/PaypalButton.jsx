d13c6de48443d33890287e6e7f47c1d976dce2c8
Wilialberth Jiménez <7jimenez.w@gmail.com>
2023-08-09 01:30:04 -0300

    Agrego datos.

--- src/components/PaypalButton/PaypalButton.jsx
+++ src/components/PaypalButton/PaypalButton.jsx
@@ -1,4 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
+
+export function PayPalButton({ precio, nombre }) {
+  const clientId = "AVEb_6nyKhKDoS1J27I2AiAdYH3r0qD-GUwkHptOALusT7-kTlhjgG8adJFy39QVrt80CiozaAFb6P3u";
+
+  const [purchaseId, setPurchaseId] = useState(null); // Estado para almacenar la ID de compra
+
+  // Manejar la lógica después de un pago exitoso
+  const handlePaymentSuccess = (details, data) => {
+    console.log("Pago realizado con éxito:", details);
+    setPurchaseId(details.purchase_units[0].payments.captures[0].id); // Asigna la ID de compra
+  };
+
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '75vh' }}>
+      <PayPalScriptProvider options={{ "client-id": clientId }}>
+        <div style={{ width: '50%' }} >
+          {purchaseId ? (
+            <div>
+              <h2>Seña exitosa.
+                Gracias por elegirnos.
+              </h2>
+              <p>ID de seña: {purchaseId} </p>
+            </div>
+          ) : (
+            <div>
+              <h2>Pago con PayPal</h2>
+              <PayPalButtons
+                createOrder={(_data, actions) => {
+                  // Lógica para crear la orden de pago
+                  return actions.order.create({
+                    application_context: {
+                      // Orden exitosa
+                      return_url: "http://localhost:3000/complete",
+                      // Orden cancelada
+                      cancel_url: "http://localhost:3000/cancel",
+                    },
+                    purchase_units: [
+                      {
+                        reference_id: "Compra de prueba ",
+                        description: `Compra de ${nombre}`,
+                        amount: {
+                          value: 200,
+                          item_total: {
+                            value: precio, // Cambiar al valor que desees cobrar
+                          },
+                        },
+                      },
+                    ],
+                  });
+                }}
+                onApprove={(data, actions) => {
+                  // Lógica después de que el usuario apruebe el pago
+                  return actions.order.capture().then(function (details) {
+                    handlePaymentSuccess(details, data);
+                  });
+                }}
+              />
+            </div>
+          )}
+        </div>
+      </PayPalScriptProvider>
+    </div>
+  );
+}
+export default PayPalButton;
+ 
+
+
+/* import React from 'react';
 import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
 
 export function PayPalButton({ precio, nombre }) {
@@ -50,11 +120,7 @@ export function PayPalButton({ precio, nombre }) {
 }
 
 export default PayPalButton;
-
-
-
-
-
+ */
 
 
 /* import React from 'react';
