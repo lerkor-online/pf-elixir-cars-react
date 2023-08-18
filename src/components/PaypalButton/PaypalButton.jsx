7f1d2a7194f1975c81071ffc8eac76744f2f0bc6
Wilialberth Jiménez <7jimenez.w@gmail.com>
2023-08-18 01:08:07 -0300

    Agrego img compra finalizada

--- src/components/PaypalButton/PaypalButton.jsx
+++ src/components/PaypalButton/PaypalButton.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
 import { useParams } from 'react-router-dom';
+import compra from '../../assets/compra_paypal.png'
 
 export function PayPalButton() {
   const clientId = "AVEb_6nyKhKDoS1J27I2AiAdYH3r0qD-GUwkHptOALusT7-kTlhjgG8adJFy39QVrt80CiozaAFb6P3u";
@@ -8,16 +9,14 @@ export function PayPalButton() {
   const [purchaseId, setPurchaseId] = useState(null);
   const { precio, nombre } = useParams();
   const [isCancelled, setIsCancelled] = useState(false);
-  const [isCompleted, setIsCompleted] = useState(false); // Estado para manejar la finalización de la compra
+  const [isCompleted, setIsCompleted] = useState(false);
 
-  // Manejar la lógica después de un pago exitoso
   const handlePaymentSuccess = (details, data) => {
     console.log("Pago realizado con éxito:", details);
     setPurchaseId(details.purchase_units[0].payments.captures[0].id);
-    setIsCompleted(true); // Marcar la compra como completada
+    setIsCompleted(true);
   };
 
-  // Manejar la cancelación de la compra
   const handleCancel = () => {
     setIsCancelled(true);
   };
@@ -25,18 +24,21 @@ export function PayPalButton() {
   return (
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '75vh' }}>
       <PayPalScriptProvider options={{ "client-id": clientId }}>
-        <div style={{ width: '50%' }} >
+        <div style={{ width: '50%' }}>
           {isCancelled ? (
             <div>
               <h2>Compra cancelada.</h2>
               <p>Has cancelado la compra de {nombre}.</p>
             </div>
           ) : isCompleted ? (
-            <div style={{ textAlign: 'center', backgroundColor: '#dff0d8', border: '1px solid #c3e6cb', padding: '20px', borderRadius: '5px' }}>
-              <h2 style={{ color: '#28a745' }}>¡Compra completada!</h2>
-              <p style={{ color: '#333' }}>Tu compra de {nombre} ha sido exitosa.</p>
-              <p style={{ color: '#333' }}>ID de compra: {purchaseId}</p>
+            <div style={{ textAlign: 'center', backgroundColor: '#dff0d8', border: '1px solid #c3e6cb',  borderRadius: '5px' }}>
+              <h2 style={{ color: '#28a745' }}>¡Seña completada!</h2>
+              <p style={{ color: '#333' }}>Tu seña de {nombre} ha sido exitosa.</p>
+              <p style={{ color: '#333' }}>ID de seña: {purchaseId}</p>
               <p style={{ color: '#333' }}>Gracias por elegirnos.</p>
+              <div style={{ display: 'flex', justifyContent: 'center', marginTop: '20px' }}>
+                <img src={compra} alt="Felicidad" style={{ Width: '100px',  }} />
+              </div>
             </div>
           ) : (
             <div>
@@ -74,6 +76,9 @@ export function PayPalButton() {
 }
 
 export default PayPalButton;
+
+
+
 
 // Código con botón de débito/crédito quitado
 /* import React, { useState } from 'react';
